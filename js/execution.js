dbf88e7761e486b19cc164656fb7d887e65ae724
DrDue <70370132+DrDue@users.noreply.github.com>
2020-09-16 15:47:06 +0200

    initial

---
+++ js/execution.js
@@ -0,0 +1,8 @@
+'use strict';
+document.write( `<h1>Assignments</h1>` );
+
+document.write( `<h2>DE.5.0</h2>` );
+
+document.write(text);
+
+document.write( `<hr/>` );
---
+++ js/js.js
@@ -0,0 +1,8 @@
+'use strict'
+
+let placesToVisit = ['New York', 'San Francisco', 'Miami', 'Las Vegas', 'Washington Dc'];
+
+var i, len, text;
+for (i = 0, len = placesToVisit.length, text = ""; i < len; i++) {
+  text += placesToVisit[i] + "<br>";
+}
